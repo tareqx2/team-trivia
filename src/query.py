0ff90b1b8219ef5ba7888f0a1eb9c7e177a118e8
Tareq Perez <tareqx3@gmail.com>
2015-01-19 13:27:49 -0600

    added some routes

--- src/query.py
+++ src/query.py
@@ -1,9 +1,49 @@
 import db
-import flask
+from flask import Flask, jsonify
+from flask import abort
+from flask import request
 
-def get_questions(count=1000):
-	questions = db.session.query(db.Documents).limit(count)
-	return questions
+app = Flask(__name__)
 
+@app.route('/api/v1.0/questions', methods=['GET'])
+def get_questions():
+	count = request.args.get('count') if request.args.get('count') else 0
+	if count==0
+		abort(400)
+	questions = db.session.query(db.Documents).limit(count) if count != 0 else db.session.query(db.Documents)
+	return jsonify({'questions',questions.clues}),200
 
-questions = get_questions()
+@app.route('/api/v1.0/questions/<int:id>', methods=['GET'])
+def get_questions(id):
+	questions = db.session.query(db.Documents).\
+    				filter(documents.id == id)
+	return jsonify({'questions',questions.clues}),200
+
+@app.route('/api/v1.0/questions', methods=['POST'])
+def add_questions():
+	if not request.json or not 'question' in request.json:
+        abort(400)
+    abort(500)
+
+@app.route('/api/v1.0/scores/', methods=['GET'])
+def get_scores():
+	scores = db.session.query(db.Scores)
+	return jsonify({'scores',scores}),200
+
+@app.route('/api/v1.0/scores/<int:userid>', methods=['GET'])
+def get_scores(userid):
+	scores = db.session.query(db.Scores).\
+				filter(scores.userid == userid)
+	return jsonify({'scores',scores}),200
+
+@app.route('/api/v1.0/scores/<int:userid>', methods=['PUT'])
+def update_scores(userid):
+	 if not request.json or not 'score' in request.json:
+        abort(400)
+	scores = db.session.query(db.Scores).\
+				filter(scores.userid == userid).\
+    			update({"score": request.json['score']})
+    return jsonify({'scores',scores}),201
+
+if __name__ == '__main__':
+    app.run()
